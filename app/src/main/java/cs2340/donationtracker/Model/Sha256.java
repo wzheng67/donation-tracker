46c87afe0b8cbfbf5734b4e4680c1c4cc2f9f4a8
kodw0402 <daewoong.ko@gatech.edu>
2018-10-13 14:56:14 -0400

    I made gradle dependencies up to date

---
+++ app/src/main/java/cs2340/donationtracker/Model/Sha256.java
@@ -0,0 +1,35 @@
+package cs2340.donationtracker.Model;
+
+// This source from http://blog.kindler.io/java-encrypt/
+
+
+import java.security.MessageDigest;
+
+public class Sha256 {   // this class encrypt a String with the system SHA256.
+    public static String encrypt(String planText) {
+        try{
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(planText.getBytes());
+            byte byteData[] = md.digest();
+
+            StringBuffer sb = new StringBuffer();
+            for (int i = 0; i < byteData.length; i++) {
+                sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+            }
+
+            StringBuffer hexString = new StringBuffer();
+            for (int i=0;i<byteData.length;i++) {
+                String hex=Integer.toHexString(0xff & byteData[i]);
+                if(hex.length()==1){
+                    hexString.append('0');
+                }
+                hexString.append(hex);
+            }
+
+            return hexString.toString();
+        }catch(Exception e){
+            e.printStackTrace();
+            throw new RuntimeException();
+        }
+    }
+}
