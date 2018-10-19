0a1bfeeec8aa70785a517ab22c9e9071ff8bf510
Richard Meng <richardmeng678@gmail.com>
2018-10-18 22:47:21 -0400

    add the enum class for category

--- app/src/main/java/cs2340/donationtracker/Model/Category.java
+++ app/src/main/java/cs2340/donationtracker/Model/Category.java
@@ -1,5 +1,13 @@
 package cs2340.donationtracker.Model;
 
 public enum Category {
-    Clothing, Hat, Kitchen, Electronics, Household, Other
+    CLOTHING("Clothing"), HAT("Hat"), KITCHEN("Kitchen"), EKECTRONICS("Electronics"),
+    HOUSEHOLD("Household"), OTHER("Other");
+
+    private String category;
+
+    Category(String s) {this.category = s}
+
+    @Override
+    public String toString() {return category;}
 }
