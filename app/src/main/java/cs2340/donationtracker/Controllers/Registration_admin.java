21303bd2f58beb722d4e2b37ac230814daad73dc
kodw0402 <daewoong.ko@gatech.edu>
2018-10-23 01:33:33 -0400

    I made test branch

---
+++ app/src/main/java/cs2340/donationtracker/Controllers/Registration_admin.java
@@ -0,0 +1,16 @@
+package cs2340.donationtracker.Controllers;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import cs2340.donationtracker.R;
+
+public class Registation_admin extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registation_admin);
+    }
+
+}
---
+++ app/src/main/java/cs2340/donationtracker/Controllers/Registration_type.java
@@ -0,0 +1,14 @@
+package cs2340.donationtracker;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class registration_type extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration_type);
+    }
+
+}
